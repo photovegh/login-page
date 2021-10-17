b750d7f9169733a5f99140f8b5ced62330f71f1f
Tibor Végh <73848390+photovegh@users.noreply.github.com>
2021-10-17 17:44:10 +0200

    HTTP server with ExpressJS begin

---
+++ index-login.js
@@ -0,0 +1,12 @@
+import express from 'express';
+//import bodyParser from "body-parser";
+var app = express();
+app.set('port', process.env.PORT || 9999);
+
+app.get('/', (req, res) => {
+    res.send('Hello World! 😋')
+  })
+
+app.listen(app.get('port'), function () {
+    console.log('Az expressz elindult a http: // localhost:' + app.get('port') + ' helyen; a Ctrl-C megnyomásával zárja be a szervert.');
+});
