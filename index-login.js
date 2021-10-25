b801c9229213cb032fb64675b692f68f4e0dc71c
Tibor Végh <73848390+photovegh@users.noreply.github.com>
2021-10-25 10:47:52 +0200

    MogoDB DigitalOcean connect OK - a DigitalOcean default NodeJS 10-es ver. lecserélve 14-re mivel a mongoose 4-et nem támogatja a régi, csak a 3.6-ot ami ultra gáz. (mátmint a DO-tól, hogy a teszteléshez 10-es Node-ot rak fel az 5$-os serverhez - kíváncsi lennék ha NodeServert futtatna élesben az milyen verziójú lenne? 😊)

--- index-login.js
+++ index-login.js
@@ -1,24 +1,54 @@
 //import path from 'path';
 
-const express = require('express');
+const express = require("express");
 const bodyParser = require("body-parser");
 const fs = require("fs");
-const path = require('path')
+const path = require("path");
 const app = express();
-path
+
+path;
 app.use(bodyParser.json());
-app.use(express.static("public"))
-app.use(express.static("public/js"))
-app.use(express.static("public/css"))
-app.use(express.static("public/img"))
-app.set('port', process.env.PORT || 9999);
+app.use(express.static("public"));
+app.use(express.static("public/js"));
+app.use(express.static("public/css"));
+app.use(express.static("public/img"));
+app.set("port", process.env.PORT || 9999);
 
-  app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '/views/index.html'));
-    //res.sendFile('i:/test/login-page/views/index.html');
-    //res.send(console.log(path.join(__dirname, '/views/index.html')));
-  })
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "/views/index.html"));
+  //res.sendFile('i:/test/login-page/views/index.html');
+  //res.send(console.log(path.join(__dirname, '/views/index.html')));
+  //res.send(res.send('Hello World! 😊 *** ROOT ***'));
+});
 
-app.listen(app.get('port'), function () {
-    console.log('Az expressz elindult a http: // localhost:' + app.get('port') + ' helyen; a Ctrl-C megnyomásával zárja be a szervert.');
+app.get("/person", (req, res) => {
+  /* 
+    res.send(res.send('Hello World! 😊   *** PERSON ***'));
+     */
+  /* res.send(console.log('app.get/person is ok index login')); */
+
+  const { MongoClient } = require("mongodb");
+  const uri =
+    "mongodb+srv://photovegh:Sususoft_0913@cluster0.gfjvb.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+  const client = new MongoClient(uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
+  client.connect(async (err) => {
+    const collection = client.db("login_page").collection("person");
+    // perform actions on the collection object
+    const person = await collection.find().toArray();
+    client.close();
+
+    res.send(person);
+    console.log(person);
+  });
+});
+
+app.listen(app.get("port"), function () {
+  console.log(
+    "Az expressz elindult a http: // localhost:" +
+      app.get("port") +
+      " helyen; a Ctrl-C megnyomásával zárja be a szervert."
+  );
 });
--- public/js/script.js
+++ public/js/script.js
@@ -1,3 +1,21 @@
-const myFunction = () => {
-    console.log('script.js is ready');
+const modalLogin = () => {
+  console.log("modalLogin is ready *** modal OK + MongoDB ***");
 };
+
+const mixedLogin = () => {
+  console.log("mixedLogin is ready !!! js FOLDER !!!");
+};
+
+function veghSoft() {
+  console.log("OK");
+  window.location = "http://www.veghsoft.eu";
+}
+
+const personGet = () => {
+  console.log("GET personGet function OK !!!!!");
+  window.location = "http://64.227.121.167:9999/person";
+};
+
+const formaz = (person) => {
+  document.body = (person + "Hello World")
+}
